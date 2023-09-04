99f06b027ab33542a587e7bc708dea1aaffcbf42
rcdavis <rcdavisgd@gmail.com>
2023-09-03 21:59:03 -0400

    Writing perks to xml files working

--- WebParsing.py
+++ WebParsing.py
@@ -1,42 +1,26 @@
 from bs4 import BeautifulSoup
 
-def parseHtml(htmlStr: str):
-    #perksHtml = BeautifulSoup(htmlStr, 'html.parser')
+def parsePerkPage(htmlFile: str, locale = ''):
+    with open(htmlFile, "r", encoding="utf8") as f:
+        perks = getStringDefinitions(f.read(), "div.formattedPerkDesc")
+        writeOutPerkNames("perkNames" + locale + ".xml", perks)
+        writeOutPerkDescs("perkDescriptions" + locale + ".xml", perks)
 
-    """
-    with open("strings.xml", "w", encoding="utf8") as w:
+def writeOutPerkNames(file: str, perks: dict[str, str]):
+    with open(file, "w", encoding="utf8") as w:
         w.write('<?xml version="1.0" encoding="utf-8"?>\n')
         w.write("<resources>\n")
-        for desc in perksHtml.select("div.formattedPerkDesc"):
-            perkName = desc.find_parent("td").find_parent("tr").select("th")
-            print("perkName = " + perkName)
-            w.write("<string name=\"" + perkName + "\">" + desc.text + "</string>\n")
+        for name in perks.keys():
+            w.write("    <string name=\"" + createPerkStringName(name, "_name") + "\">" + sanitizeText(name) + "</string>\n")
         w.write("</resources>\n")
-    """
 
-    """
-    with open("strings.xml", "w", encoding="utf8") as w:
-        w.write('<?xml version="1.0" encoding="utf-8"?>\n')
-        w.write("<resources>\n")
-        for tableRow in perksHtml.select("tbody > tr"):
-            heading = tableRow.find("th").find("a")
-            perkName = heading['title']
-            descriptions = tableRow.select("div.formattedPerkDesc")
-            if descriptions.count == 0:
-                continue
-            w.write("<string name=\"" + createDescStringName(perkName) + "\">" + sanitizeText(descriptions[0].text) + "</string>\n")
-        w.write("</resources>\n")
-    """
 
-    #writeOutDescs("rawStrings.xml", getStringDefinitions(htmlStr, "div.rawPerkDesc"))
-    writeOutDescs("formattedStrings.xml", getStringDefinitions(htmlStr, "div.formattedPerkDesc"))
-
-def writeOutDescs(file: str, descs: dict[str, str]):
+def writeOutPerkDescs(file: str, descs: dict[str, str]):
     with open(file, "w", encoding="utf8") as w:
         w.write('<?xml version="1.0" encoding="utf-8"?>\n')
         w.write("<resources>\n")
         for name, text in descs.items():
-            w.write("<string name=\"" + name + "\">" + text + "</string>\n")
+            w.write("    <string name=\"" + createPerkStringName(name, "_desc") + "\">" + sanitizeText(text) + "</string>\n")
         w.write("</resources>\n")
 
 
@@ -52,18 +36,19 @@ def getStringDefinitions(htmlStr: str, descType = "div.formattedPerkDesc") -> di
             if len(descriptions) < 1:
                 continue
 
-            results[createDescStringName(perkName)] = sanitizeText(descriptions[0].text)
+            results[perkName] = descriptions[0].text
         except:
             print("Error getting string definition")
 
     return results
 
 def sanitizeText(text: str) -> str:
-    return text.replace(' ', '').replace('&', '&amp;').replace('"', '&quot;').replace("'", '&apos;')
+    return text.replace(' ', '').replace('&', '&amp;').replace('"', '\\"').replace("'", "\\'").replace('\n', '\\n')
 
-def createDescStringName(text: str) -> str:
-    nameStr = 'perk_' + text.lower().replace(':', '').replace('!', '').replace("'", '')
-    return nameStr.replace('-', '_').replace(' ', '_').replace('é', 'e').replace('à', 'a').replace('&', 'and') + '_desc'
+def createPerkStringName(text: str, endingName: str) -> str:
+    nameStr = 'perk_' + text.lower().replace(':', '').replace('!', '').replace("'", "")
+    nameStr = nameStr.replace('-', '_').replace(' ', '_').replace('é', 'e').replace('à', 'a').replace('&', 'and')
+    return nameStr + endingName
 
-with open("Perks.html", "r", encoding="utf8") as f:
-    parseHtml(f.read())
+
+parsePerkPage("Perks_en.html")
